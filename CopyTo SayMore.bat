c69494b0f377f60f21fb3addaccd3ee26673b048
gtryus <gtryus@gmail.com>
2018-02-16 11:34:30 -0600

    Update Saymore copy script

--- CopyTo SayMore.bat
+++ CopyTo SayMore.bat
@@ -1,16 +1,29 @@
 set sayMoreDir=..\SayMore
-copy /Y output\debug\Palaso.dll %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.xml %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.pdb %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.TestUtilities.* %sayMoreDir%\lib\dotnet
-copy /Y output\debug\PalasoUIWindowsForms.dll %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.Media.dll %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.Media.xml %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.Media.pdb %sayMoreDir%\lib\dotnet
-copy /Y output\debug\PalasoUIWindowsForms.xml %sayMoreDir%\lib\dotnet
-copy /Y output\debug\PalasoUIWindowsForms.pdb %sayMoreDir%\lib\dotnet
-copy /Y output\debug\Palaso.BuildTasks.dll %sayMoreDir%\build
+copy /Y output\debug\SIL.Core.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Core.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Core.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Core.dll.config %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Archiving.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Archiving.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Archiving.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Archiving.dll.config %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Media.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Media.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Media.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Media.dll.config %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.dll.config %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.WritingSystems.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.WritingSystems.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.Windows.Forms.WritingSystems.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.WritingSystems.dll %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.WritingSystems.xml %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.WritingSystems.pdb %sayMoreDir%\lib\dotnet
+copy /Y output\debug\SIL.BuildTasks.dll %sayMoreDir%\build
+copy /Y output\debug\SIL.BuildTasks.pdb %sayMoreDir%\build
 
-copy /Y %sayMoreDir%\lib\dotnet\Palaso*.* %sayMoreDir%\output\debug
+copy /Y %sayMoreDir%\lib\dotnet\SIL*.* %sayMoreDir%\output\debug
 
 pause
